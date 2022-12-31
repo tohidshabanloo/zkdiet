2b1d71d88ddbe059fd78f8df2681042d3204ba37
tohidshabanloo <tshabanloo@hotmail.com>
2022-12-31 06:44:00 -0800

    font changed

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -1,6 +1,7 @@
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import "../../styles/globals.css";
+import "../../styles/font.css";
 
 export default function RootLayout({
   children,
