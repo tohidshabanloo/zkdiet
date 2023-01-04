2899ca3b756e6e286f264e4c6d742a3a33c90ebf
tohidshabanloo <tshabanloo@hotmail.com>
2023-01-04 02:53:00 -0800

    change style banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -10,20 +10,22 @@ type Props = {
 
 const Banner = ({ bannerData }: Props) => {
   return (
-    <section className="grid lg:grid-cols-6 space-y-5 p-2 md:p-4">
+    <section className="gap-4 grid lg:grid-cols-6 md:grid-cols-6 space-y-5 p-2 md:p-4">
       {/* Right */}
-      <div className="space-y-4 col-span-3">
-        <h1 className="text-4xl font-bold">{bannerData.hedaer1}</h1>
-        <h2 className="font-semibold text-gray-500 tracking-widetext-sm md:text-base text-justify">
+      <div className="space-y-4 col-span-3 md:col-span-3 lg:mt-4 md:mt-4">
+        <h1 className="text-3xl lg:text-2xl md:text-lg font-bold">
+          {bannerData.hedaer1}
+        </h1>
+        <h2 className="font-semibold text-gray-500 tracking-widetext-sm md:text-xs  text-justify">
           {bannerData.header2}
         </h2>
-        <h2 className="text-gray-400 text-sm md:text-base font-bold text-justify">
+        <h2 className="text-gray-400 text-sm md:text-xs font-bold text-justify">
           {bannerData.description}
         </h2>
       </div>
 
       {/* Left */}
-      <div className="col-span-3 mr-4 sm:grid">
+      <div className="col-span-3 md:col-span-3  sm:grid">
         <CoverImage
           image={bannerData.bannerImage.url}
           title={undefined}
