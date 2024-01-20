66100cd76530db027751129a3be544f7d699700e
tohidshabanloo <tshabanloo@hotmail.com>
2024-01-20 08:31:01 -0800

    added authorimage 4

--- schemas/authorphoto.ts
+++ schemas/authorphoto.ts
@@ -2,7 +2,7 @@ import { defineField, defineType } from "sanity";
 
 export default defineType({
   name: "authorimage",
-  title: "بنر",
+  title: "مشخصات نویسنده",
   type: "document",
   fields: [
     defineField({
