e509b3bcd5f0ba1bfdc03225eb67cd160ff472d3
tohidshabanloo <tshabanloo@hotmail.com>
2024-01-20 07:51:24 -0800

    added authorimage 2

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,7 +1,15 @@
 import blockContent from "./blockContent";
 import category from "./category";
+import authorphoto from "./authorphoto";
 import post from "./post";
 import author from "./author";
 import banner from "./banner";
 
-export const schemaTypes = [post, author, category, blockContent, banner];
+export const schemaTypes = [
+  post,
+  author,
+  category,
+  blockContent,
+  banner,
+  authorphoto,
+];
