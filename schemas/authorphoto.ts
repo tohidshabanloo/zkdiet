ba3245abf6610583119e1bf41acf1fb21a7c187b
tohidshabanloo <tshabanloo@hotmail.com>
2024-01-20 08:39:44 -0800

    added authorimage 6

--- schemas/authorphoto.ts
+++ schemas/authorphoto.ts
@@ -6,6 +6,11 @@ export default defineType({
   type: "document",
   fields: [
     defineField({
+      name: "title",
+      title: "اسم نویسنده",
+      type: "string",
+    }),
+    defineField({
       name: "description",
       title: "توضیحات",
       type: "string",
