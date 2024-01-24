e0c1d47571fd9f277db637789884e262db025ec3
tohidshabanloo <tshabanloo@hotmail.com>
2024-01-24 01:49:59 -0800

    added instagram fram

--- components/VideoSlider.js
+++ components/VideoSlider.js
@@ -25,46 +25,64 @@ const VideoSlider = ({ videos }) => {
     onSlideChange: handleSlideChange,
     breakpoints: {
       400: {
-        slidesPerView: 1.2, // Show 1 slide per view on screens smaller than 768px (e.g., mobile)
+        slidesPerView: 3, // Show 1 slide per view on screens smaller than 768px (e.g., mobile)
       },
       768: {
-        slidesPerView: 2.2, // Show 1 slide per view on screens smaller than 768px (e.g., mobile)
+        slidesPerView: 3, // Show 1 slide per view on screens smaller than 768px (e.g., mobile)
       },
       992: {
-        slidesPerView: 2.5, // Show 2 slides per view on screens between 768px and 992px
+        slidesPerView: 3, // Show 2 slides per view on screens between 768px and 992px
       },
       1200: {
-        slidesPerView: 3.5, // Show 3 slides per view on screens larger than 992px
+        slidesPerView: 3, // Show 3 slides per view on screens larger than 992px
       },
     },
   };
 
   return (
-    <div className="p-20">
-      <div className={styles.videoSliderContainer}>
-        <img
-          className={styles.instagramFrame}
-          src="/images/instagram-frame2.png"
-          alt="Instagram Frame"
-        />
-        <Swiper {...swiperParams}>
-          {videos.map((video, index) => (
-            <SwiperSlide key={index}>
-              <div
-                className={`${styles.videoWrapper} ${
-                  activeIndex === index ? styles.active : ""
-                }`}
-              >
-                <video controls width="100%">
-                  <source src={video.src} type="video/mp4" />
-                  Your browser does not support the video tag.
-                </video>
-              </div>
-            </SwiperSlide>
-          ))}
-          <div className="swiper-button-next"></div>
-          <div className="swiper-button-prev"></div>
-        </Swiper>
+    <div className="-z-1 flex items-center justify-center h-screen ">
+      <div className="w-9/12 max-w-3xl  rounded-lg shadow-md ">
+        <div className={styles.videoSliderContainer}>
+          <div className="flex  items-center bg-gray-600 p-4">
+            <img
+              src="https://source.unsplash.com/random"
+              alt="Profile"
+              className="w-10 h-10 rounded-full mr-4"
+            />
+            <h1 className="text-xl font-bold ">Username</h1>
+          </div>
+          <div className="absolute p-4 bg-gray-600  mt-96">
+            <div className="flex items-center  mb-2">
+              <img
+                src="https://source.unsplash.com/random"
+                alt="Profile"
+                className="w-8 h-8 rounded-full mr-2"
+              />
+              <h2 className="text-lg font-semibold">Username</h2>
+            </div>
+            <p className="text-sm">Caption</p>
+          </div>
+          <div className="">
+            <Swiper {...swiperParams}>
+              {videos.map((video, index) => (
+                <SwiperSlide key={index}>
+                  <div
+                    className={`${styles.videoWrapper} ${
+                      activeIndex === index ? styles.active : ""
+                    }`}
+                  >
+                    <video controls width="100%">
+                      <source src={video.src} type="video/mp4" />
+                      Your browser does not support the video tag.
+                    </video>
+                  </div>
+                </SwiperSlide>
+              ))}
+              <div className="swiper-button-next"></div>
+              <div className="swiper-button-prev"></div>
+            </Swiper>
+          </div>
+        </div>
       </div>
     </div>
   );
