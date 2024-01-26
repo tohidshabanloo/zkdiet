5850fd11a82d627a818313dc2ddcba0e9efc4f82
tohidshabanloo <tshabanloo@hotmail.com>
2024-01-26 15:06:49 -0800

    frame size fixed

--- components/VideoSlider.js
+++ components/VideoSlider.js
@@ -25,43 +25,35 @@ const VideoSlider = ({ videos }) => {
     onSlideChange: handleSlideChange,
     breakpoints: {
       400: {
-        slidesPerView: 3, // Show 1 slide per view on screens smaller than 768px (e.g., mobile)
+        slidesPerView: 1, // Show 1 slide per view on screens smaller than 768px (e.g., mobile)
       },
       768: {
-        slidesPerView: 3, // Show 1 slide per view on screens smaller than 768px (e.g., mobile)
+        slidesPerView: 2.9, // Show 1 slide per view on screens smaller than 768px (e.g., mobile)
       },
       992: {
-        slidesPerView: 3, // Show 2 slides per view on screens between 768px and 992px
+        slidesPerView: 2.9, // Show 2 slides per view on screens between 768px and 992px
       },
       1200: {
-        slidesPerView: 3, // Show 3 slides per view on screens larger than 992px
+        slidesPerView: 2.9, // Show 3 slides per view on screens larger than 992px
       },
     },
   };
 
   return (
-    <div className="-z-1 flex items-center justify-center h-screen ">
-      <div className="w-9/12 max-w-3xl  rounded-lg shadow-md ">
-        <div className={styles.videoSliderContainer}>
-          <div className="flex  items-center bg-gray-600 p-4">
-            <img
-              src="https://source.unsplash.com/random"
-              alt="Profile"
-              className="w-10 h-10 rounded-full mr-4"
-            />
-            <h1 className="text-xl font-bold ">Username</h1>
-          </div>
-          <div className="absolute p-4 bg-gray-600  mt-96">
-            <div className="flex items-center  mb-2">
+    <div className="flex items-center justify-center ">
+      <div className="w-full max-w-5xl p-4 rounded-lg shadow-md ">
+        <div>
+          <div className="flex justify-center">
+            <div className="w-full md:w-1/3 lg:w-1/3  items-center bg-gray-600 ">
               <img
                 src="https://source.unsplash.com/random"
                 alt="Profile"
-                className="w-8 h-8 rounded-full mr-2"
+                className="w-10 h-10 rounded-full mr-4"
               />
-              <h2 className="text-lg font-semibold">Username</h2>
+              <h1 className="text-xl font-bold ">Username</h1>
             </div>
-            <p className="text-sm">Caption</p>
           </div>
+
           <div className="">
             <Swiper {...swiperParams}>
               {videos.map((video, index) => (
@@ -81,6 +73,19 @@ const VideoSlider = ({ videos }) => {
               <div className="swiper-button-next"></div>
               <div className="swiper-button-prev"></div>
             </Swiper>
+            <div className="flex justify-center">
+              <div className="w-full md:w-1/3 lg:w-1/3 flex p-4 bg-gray-600">
+                <div className="flex items-center  mb-2">
+                  <img
+                    src="https://source.unsplash.com/random"
+                    alt="Profile"
+                    className="w-8 h-8 rounded-full mr-2"
+                  />
+                  <h2 className="text-lg font-semibold">Username payin</h2>
+                </div>
+                <p className="text-sm">Caption payin</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
